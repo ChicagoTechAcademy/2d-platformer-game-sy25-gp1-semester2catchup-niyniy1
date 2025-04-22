bc4b7bee1e7ca686cf5538b554557a4ce805d51e
niyniy1 <143821354+niyniy1@users.noreply.github.com>
2025-04-22 09:49:17 -0500

    done

--- Assets/Scripts/Gamemanager.cs
+++ Assets/Scripts/Gamemanager.cs
@@ -1,11 +1,17 @@
+using System;
 using UnityEngine;
+using TMPro;
 
 public class Gamemanager : MonoBehaviour
 {
+    
+    [SerializeField]private int playerHealth = 3;
+    [SerializeField]private TextMeshProUGUI healthText;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
+    { 
+        healthText.text = "Health: " + playerHealth.ToString();   
     }
 
     // Update is called once per frame
